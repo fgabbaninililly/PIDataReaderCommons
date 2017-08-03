451f24450c35c50111cc9c334182abc6f18fe652
fgabbaninililly <gabbanini_francesco@lilly.com>
2017-08-03 12:27:56 +0200

    Added version file

---
+++ Version.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIDataReaderCommons {
+	public class Version {
+		public static readonly string version = "1.0.0";
+
+		public static string getVersion() {
+			return string.Format("PIDataReaderCommons version {0}", version);
+		}
+	}
+}
