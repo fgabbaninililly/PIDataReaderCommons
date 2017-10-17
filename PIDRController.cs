7a41a2d0803644eb9b53f8901b76a7d9c6cc54b9
fgabbaninililly <gabbanini_francesco@lilly.com>
2017-10-17 15:44:03 +0200

    Can use MQTT publishers implemented with different libraries (M2MQTT and MQTTNet).

--- PIDRController.cs
+++ PIDRController.cs
@@ -20,7 +20,7 @@ namespace PIDataReaderCommons {
 
 		private PIDRContext pidrContext;
 		private Reader reader;
-		private MQTTWriter mqttWriter;
+		private AbstractMQTTWriter mqttWriter;
 		private FileWriter fileWriter;
 		private Options options;
 
@@ -125,6 +125,7 @@ namespace PIDataReaderCommons {
 
 			mqttWriter.MQTTWriter_PublishCompleted += MQTTWriter_PublishCompleted;
 			mqttWriter.MQTTWriter_ClientClosed += MqttWriter_MQTTWriter_ClientClosed;
+
 			logger.Info("MQTT data writer was successfully created");
 			logger.Info("MQTT client ID is {0}", mqttWriter.getClientName());
 
@@ -269,7 +270,7 @@ namespace PIDataReaderCommons {
 
 			logger.Info("Total time required for publish: {0}s", e.elapsedTimeSec.ToString());
 			logger.Info("Total time required for reading and publishing: {0}s", totalReadAndPublishTimeSec);
-			logger.Info("Throughput in this scheduled run was: {0} bytes/s", e.throughput.ToString("F2"));
+			logger.Info("Network throughput in this run was: {0} bytes/s", e.throughput.ToString("F2"));
 			PIReaderConfig config = pidrContext.getConfig();
 			if (config.read.readExtent.type.ToLower().Equals(ReadExtent.READ_EXTENT_FREQUENCY)) {
 				double scheduleAsSec = config.read.readExtent.readExtentFrequency.getFrequencySecondsAsDouble();
