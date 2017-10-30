e6f4ec6a607db4223db038d504fbf7637d75bf2e
fgabbaninililly <gabbanini_francesco@lilly.com>
2017-10-30 14:33:38 +0100

    Bump version to 2.2.0

--- Version.cs
+++ Version.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace PIDataReaderCommons {
 	public class Version {
-		public static readonly string version = "2.1.0";
+		public static readonly string version = "2.2.0";
 
 		public static string getVersion() {
 			return string.Format("PIDataReader Common Classes v{0}", version);
